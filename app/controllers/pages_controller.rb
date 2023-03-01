97a0a75b19b9ca4d5a32472827e997458a36ab79
Anthony Francis <tonytrack@gmail.com>
2023-03-01 23:05:38 +0000

    side project sidbar links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
+  before_action :set_project, only: %i[ show edit update destroy ]
+  
   def index
+    @project_sidebar = Project.all
   end
 
   def people
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1 or /projects/1.json
   def show
+    @projects = Project.all
   end
 
   # GET /projects/new
